9edeeeb4f8828df834437832185ef3b3a88acdf1
Mazux <lachlan.andrews2@gmail.com>
2015-08-02 15:43:10 +1000

    Update _defaults.php

--- protected/config/_defaults.php
+++ protected/config/_defaults.php
@@ -3,7 +3,7 @@
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     // Default Application Name
-    'name' => 'HumHub',
+    'name' => 'Youbook',
     'preload' => array('log', 'input'),
     'components' => array(
         // Database
